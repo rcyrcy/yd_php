73e0ca5d0b43e90f6b9ba8b7506c0c38d0faa849
rcyrcy <18345059439@163.com>
2019-02-07 22:38:39 +0800

    case

---
+++ case.php
@@ -0,0 +1,61 @@
+
+<?php
+echo '[{
+    "imgurl":"img/case3.jpg",
+    "cont":"死侍2：我爱我家",
+    "pingfen":7.4,
+    "yanyuan":"大卫.雷奇 莫雷那.巴卡林 乔什.布洛林 大卫.雷奇 莫雷那.巴卡林 乔什.布洛林",
+    "country":"美国",
+    "time":119
+    },{
+    "imgurl":"img/case1.jpg",
+    "cont":"流浪地球",
+    "pingfen":"暂无",
+    "yanyuan":"郭帆 屈楚萧 李光洁 吴孟达 赵今麦",
+    "country":"中国大陆",
+    "time":100
+    },{
+    "imgurl":"img/case2.jpg",
+    "cont":"飞驰人生",
+    "pingfen":"暂无",
+    "yanyuan":"韩寒 沈腾 尹正 尹芳 天宇 黄景瑜",
+    "country":"中国大陆",
+    "time":100
+    },{
+    "imgurl":"img/case4.jpg",
+    "cont":"疯狂外星人",
+    "pingfen":"暂无",
+    "yanyuan":"宁浩 黄渤 沈腾 马修.莫里森 汤姆.拍富瑞 汤姆.莫里森",
+    "country":"中国大陆",
+    "time":100
+    },{
+    "imgurl":"img/case5.jpg",
+    "cont":"密室逃生",
+    "pingfen":7.3,
+    "yanyuan":"亚当.罗比特儿 泰勒.拉塞尔 泰勒.来伯恩 罗根.弥勒 黛博拉.安摩尔",
+    "country":"美国",
+    "time":100
+    },{
+    "imgurl":"img/case6.jpg",
+    "cont":"大黄蜂",
+    "pingfen":7.1,
+    "yanyuan":"塔拉维斯.纳特 海利.斯坦菲尔德 杰森.德鲁克 约翰.塞纳 斯坦菲尔德.安摩尔",
+    "country":"美国",
+    "time":100
+    },{
+    "imgurl":"img/case7.jpg",
+    "cont":"白蛇：缘起",
+    "pingfen":8,
+    "yanyuan":"黄嘉康 赵霁 杨天翔 唐小喜 张喆",
+    "country":"中国大陆 美国",
+    "time":95
+    },{
+    "imgurl":"img/case8.jpg",
+    "cont":"大人物",
+    "pingfen":7.9,
+    "yanyuan":"伍佰 王迅 王千源 王砚辉 包贝尔",
+    "country":"中国大陆",
+    "time":100
+    }
+]'
+?>
