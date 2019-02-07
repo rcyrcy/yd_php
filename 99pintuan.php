fa6c602a7dd8ee2f289999591ec157afbdfda2e7
rcyrcy <18345059439@163.com>
2019-02-07 22:38:17 +0800

    99

---
+++ 99pintuan.php
@@ -0,0 +1,61 @@
+<?php
+echo '[{
+    "imgurl":"../img/99case1.jpg",
+    "cont":"熊猫系列纸抽100抽*6包",
+    "cont2":"100%天然竹浆纸 不漂白 无添加 柔韧舒适",
+    "money":9.9,
+    "user":"../img/user.png",
+    "user2":"../img/user.jpg"
+    },{
+    "imgurl":"../img/99case2.jpg",
+    "cont":"万达影院观影兑换卷",
+    "cont2":"仅限湖南地区万达影院持手机APP兑换",
+    "money":23.99,
+    "user":"../img/user2.jpg",
+    "user2":"../img/user3.jpg"
+    },{
+    "imgurl":"../img/99case3.jpg",
+    "cont":"新疆薄皮臭核桃2斤",
+    "cont2":"个大肉厚饱满 皮薄易打开 口感赛鲍鱼快来买买买",
+    "money":33,
+    "user":"../img/user4.jpg",
+    "user2":"../img/user5.jpg"
+    },{
+    "imgurl":"../img/99case4.jpg",
+    "cont":"古田鲜银耳 6只装",
+    "cont2":"新鲜采摘 无需泡发 口感爽滑快来买买买",
+    "money":28,
+    "user":"../img/user6.jpg",
+    "user2":"../img/user7.jpg"
+    },{
+    "imgurl":"../img/99case5.jpg",
+    "cont":"黑金刚土豆 5斤装",
+    "cont2":"优选基地直供 紫黑发亮 软糯香甜 营养粗粮好选择",
+    "money":23,
+    "user":"../img/user8.jpg",
+    "user2":"../img/user9.jpg"
+    },{
+    "imgurl":"../img/99case6.jpg",
+    "cont":"福建鲜猴头菇 2斤",
+    "cont2":"山林手工采摘 肉质厚实爽滑 菌丝细密分布",
+    "money":36,
+    "user":"../img/user10.jpg",
+    "user2":"../img/user11.jpg"
+    },{
+    "imgurl":"../img/99case1.jpg",
+    "cont":"熊猫系列纸抽100抽*6包",
+    "cont2":"100%天然竹浆纸 不漂白 无添加 柔韧舒适",
+    "money":9.9,
+    "user":"../img/user.png",
+    "user2":"../img/user.jpg"
+    },{
+    "imgurl":"../img/99case2.jpg",
+    "cont":"万达影院观影兑换卷",
+    "cont2":"仅限湖南地区万达影院持手机APP兑换",
+    "money":23.99,
+    "user":"../img/user2.jpg",
+    "user2":"../img/user3.jpg"
+    }
+]'
+?>
+
